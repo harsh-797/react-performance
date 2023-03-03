a15c0ca2bc7ab7306a4565bae5e812446018ee93
Harshit Sinha (Sinha) <harshit.sinha@harshit.sinha-MRWRDQNVP2>
2023-03-03 13:45:43 +0530

    Completed exercise01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,8 +3,9 @@
 
 import * as React from 'react'
 // 💣 remove this import
-import Globe from '../globe'
+// import Globe from '../globe'
 
+const Globe = React.lazy(() => import(/* webpackPrefetch: true */ '../globe'))
 // 🐨 use React.lazy to create a Globe component which uses a dynamic import
 // to get the Globe component from the '../globe' module.
 
@@ -15,6 +16,16 @@ function App() {
   // with a fallback.
   // 💰 try putting it in a few different places and observe how that
   // impacts the user experience.
+
+  // function dynamicFocusLoad() {
+  //   // console.log("I'm focussing")
+  //   return import('../globe')
+  // }
+  // function dynamicMouseOverLoad() {
+  //   // console.log('my mouse pointer has entered')
+  //   return import('../globe')
+  // }
+
   return (
     <div
       style={{
@@ -26,7 +37,11 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      <label
+        style={{marginBottom: '1rem'}}
+        // onFocus={dynamicFocusLoad}
+        // onMouseEnter={dynamicMouseOverLoad}
+      >
         <input
           type="checkbox"
           checked={showGlobe}
@@ -35,7 +50,9 @@ function App() {
         {' show globe'}
       </label>
       <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
+        <React.Suspense fallback={<div>This is my fallback</div>}>
+          {showGlobe ? <Globe /> : null}
+        </React.Suspense>
       </div>
     </div>
   )
